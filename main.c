7952b9b05b0265dbe97c2fc678be63edbbfbeca7
Candango-c <sena.davi.silva@gmail.com>
2025-05-14 16:46:01 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,55 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-
-#include "produto.h"
-#include "util.h"
+#include "matriz.h"
 
 int main(){
 
-    //TADS
-    Produto *p;
+    int linhas=0, colunas=0;
+    int numero=0;
 
-    p = lerProduto();
+    printf("Indique as proporções das matrizes:\n");
 
-    NotificaProduto(p);
+    scanf("%d %d", &linhas, &colunas);
+    scanf("%*c");
 
-    desalocaProduto(p);
+    Matriz *m1 = inicializaMatriz(linhas, colunas);
+
+    Matriz *m2 = inicializaMatriz(linhas, colunas);
+
+    printf("Escreva quais numeros irão preencher a Primeira Matriz:\n");
+
+    for(int i=0; i < linhas; i++){
+        for(int j=0; j < colunas; j++){
+
+            scanf("%d", &numero);
+            scanf("%*c");
+
+            modificaElemento(m1, i, j, numero);
+
+        }
+    }
+
+    printf("Escreva quais numeros irão preencher a Segunda Matriz:\n");
+
+    for(int i=0; i < linhas; i++){
+        for(int j=0; j < colunas; j++){
+
+            scanf("%d", &numero);
+            scanf("%*c");
+
+            modificaElemento(m2, i, j, numero);
+
+        }
+    }
+
+    printf("Matriz Numero 1:\n");
+
+    imprimeMatriz(m1);
+
+    printf("Matriz Numero 2:\n");
+
+    imprimeMatriz(m2);
+
 
 
     return 0;
